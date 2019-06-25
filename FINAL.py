b63a42ca964b1e3a2abead8df3c1f36a240f1925
wesquel <51407794+wesquel@users.noreply.github.com>
2019-06-25 09:48:01 -0300

    Update FINAL.py

--- FINAL.py
+++ FINAL.py
@@ -1,28 +1,33 @@
 from copy import deepcopy
-################################################ - VARIAVEIS - #########################################################
-print('Colocar ,as entradas, separadas por espaço.')
+
+print('Colocar, as entradas, separadas por espaço.')
 entradas = input('Entradas: ').split(' ')  # Variaveis.
+
+################################################ - VARIAVEIS - #########################################################
+Tabela_Verdade = 2 ** len(entradas)  # O Numero de 0 ou 1 que vai ter por entrada.
+Repeticao = 2 ** len(entradas)  # 0 ou 1.
+
 saida = []  # Saidas.
 lista_verdade = []  # Tabela verdade nao invertida
 lista2_verdade = []  # Tabela verdade Invertida (Completa).
-Grupos = [] # Todo o Grupo / Divisão
-Saidas_Validas = [] # Saidas que Possuem 1
-Grupo_de_Simplificacão = [] # Grupo de Simplificaçao/ Reset
-Total = [] # Final dos Grupos
-gruposempar = [] # Grupos que nao Possuem Par
-Usados = [] # Elementos do Grupos Usados
-GrupoInicial= [] # Reset dos Grupos
-Grupos_Isolados = [] # Grupos sem par Separados
-Tabela_Verdade = 2 ** len(entradas)  # O Numero de 0 ou 1 que vai ter por entrada.
-Repeticao = 2 ** len(entradas)  # 0 ou 1.
+Grupos = []  # Divisão
+Saidas_Validas = []  # Saidas que Possuem 1
+Grupo_de_Simplificacão = []  # Grupo de Simplificaçao/ Reset
+Total = []  # Final dos Grupos
+gruposempar = []  # Grupos que nao Possuem Par
+Usados = []  # Elementos do Grupos Usados
+GrupoInicial = []  # Reset dos Grupos
+Grupos_Isolados = []  # Grupos sem par Separados
+
 Contagem = 0  # contagem dos grupos
-soma01 = 0 # Varivael de Soma para os reset's
-soma0 = 0 # Varivael de Soma para os reset's
-soma1 = 0 # Varivael de Soma para os reset's
-soma2 = 0 # Varivael de Soma para os reset's
-reset = 0 # Varivael de Soma para os reset's
-Soma_Dos_Grupos = 0 # Varivael de Soma para os reset's
-somaprint = 0 # Varivael de Soma para os reset's
+soma01 = 0  # Varivael de Soma para os reset's
+soma0 = 0  # Varivael de Soma para os reset's
+soma1 = 0  # Varivael de Soma para os reset's
+soma2 = 0  # Varivael de Soma para os reset's
+reset = 0  # Varivael de Soma para os reset's
+Soma_Dos_Grupos = 0  # Varivael de Soma para os reset's
+somaprint = 0  # Varivael de Soma para os reset's
+
 ######################################## - MONTAGEM DA TABELA VERDADE - ################################################
 
 for x in range(len(entradas)):  # For para as entradas.
@@ -46,18 +51,22 @@ for t in range(Tabela_Verdade):  # For pra inversão da tabela.
         modificador = lista_verdade[t1]
         lista2.append(modificador[t])
     lista2_verdade.append(lista2)
-for t3 in range(Tabela_Verdade):
-    print(lista2_verdade[t3])
 
 ################################################## - SAIDA - ###########################################################
-
-print('Saida Separada por espaços, Sua saida deve ter pelo menos', Tabela_Verdade, 'Termos')
-while (len(saida) > Tabela_Verdade) or (len(saida) < Tabela_Verdade):  # Saida.
+check = True
+print('Saída Separada por espaços, Sua saída deve ter pelo menos', Tabela_Verdade, 'Termos.')
+while (len(saida) > Tabela_Verdade) or (len(saida) < Tabela_Verdade) or (check == False):  # Saida.
+    check = True
     saida = input().split(' ')
-    if len(saida) > Tabela_Verdade:
-        print('O numero de termos na saida possui',len(saida) - Tabela_Verdade,'a mais que o permitido.')
+    for d0 in range(len(saida)):
+        if (saida[d0] != '0') and (saida[d0] != '1'):
+            check = False
+    if check == False:
+        print('A saída deve ser composta apenas com 0 ou 1, separados por espaços.')
+    elif len(saida) > Tabela_Verdade:
+        print('O número de termos na saída possui', len(saida) - Tabela_Verdade, 'a mais que o permitido.')
     elif len(saida) < Tabela_Verdade:
-        print('O numero de termos na saida possui',Tabela_Verdade - len(saida), 'a menos que o permitido.')
+        print('O número de termos na saída possui', Tabela_Verdade - len(saida), 'a menos que o permitido.')
 
 ########################################## - QUAIS SAIDAS SÃO VALIDAS - ################################################
 
@@ -67,7 +76,7 @@ for h1 in range(Tabela_Verdade):
 
 ########################################## - SAIDA SEM SIMPLIFICAÇÃO - #################################################
 check5 = True
-print('SAIDA SEM A SIMPLIFICAÇÃO: ', end='')
+print('Saida: ', end='')
 for k0 in range(len(entradas)):
     if k0 == 0:
         print('f{' + entradas[k0], end='')
@@ -76,16 +85,34 @@ for k0 in range(len(entradas)):
     else:
         print(',' + entradas[k0], end='')
 
-for k2 in range (len(saida)):
-    somaprint = somaprint + int(saida[k2])
+for k2 in range(len(saida)):
+    somaprint += int(saida[k2])
+somaentradas = 0
+if len(entradas) == 1:
+    print('} =',end=' ')
+    check5 = False
+    for d3 in range(len(saida)):
+        if saida[d3] == '1':
+            somaentradas += 1
+    if somaentradas == 2:
+        print('1')
+    elif somaentradas == 1:
+        for d4 in range(len(entradas)):
+            if saida[d4] == '1':
+                print(entradas[0]+"'")
+            elif saida[d4] == '0':
+                print(entradas[0])
+    elif somaentradas == 0:
+        print('0')
 
-if somaprint == (len(saida)):
+elif somaprint == (len(saida)):
     print('1')
     check5 = False
 elif somaprint == 0:
     print('0')
     check5 = False
 
+    check5 = False
 if check5 == True:
     for k in range(len(Saidas_Validas)):
         modificador = Saidas_Validas[k]
@@ -106,6 +133,9 @@ if check5 == True:
                         print(entradas[k1], end=' + ')
                 else:
                     print(entradas[k1], end='')
+
+if somaprint == 1:
+    check5 = False
 
 ############################################ - MONTAGEM DE GRUPOS - ####################################################
 
@@ -131,7 +161,7 @@ for j3 in range(reset + 1):  # Adicionando
 
 ################################################ - SIMPLIFICAÇÃO - #####################################################
 
-while True :
+while True:
     if soma01 != 0:
         Grupos = GrupoInicial
     for l0 in range(len(Grupos)):  # dividindo os grupos
@@ -145,19 +175,19 @@ while True :
         for l1 in range(len(modificador)):
             modificador_1 = modificador[l1]  # SUB
             if l0 < len(Grupos) - 1:
-                for l2 in range (len(Grupos[l0+1])):
-                    modificador_2 = Grupos[l0+1]
+                for l2 in range(len(Grupos[l0 + 1])):
+                    modificador_2 = Grupos[l0 + 1]
                     modificador_3 = modificador_2[l2]
                     for l3 in range(len(entradas)):
                         if modificador_1[l3] != modificador_3[l3]:
-                            soma0 = soma0 + 1
+                            soma0 += 1
                             soma1 = l3
                     if soma0 == 1:
                         Grupo_de_Simplificacão.append(deepcopy(modificador_1))
                         Usados.append(deepcopy(modificador_3))
                         Usados.append(deepcopy(modificador_1))
                         Grupo_de_Simplificacão[soma2][soma1] = '-'
-                        soma2 = soma2 + 1
+                        soma2 += 1
                     elif soma0 > 1:
                         gruposempar.append(modificador_1)
                         gruposempar.append(modificador_3)
@@ -168,7 +198,7 @@ while True :
 
 ############################################# - SEPARAÇÃO DOS GRUPOS - #################################################
 
-for g0 in range(len(gruposempar)): # Separando que nao Tem Par.
+for g0 in range(len(gruposempar)):  # Separando que nao Tem Par.
     Check_Grupos = False
     Check_Isolados = False
     for g1 in range(len(Usados)):
@@ -180,12 +210,12 @@ for g0 in range(len(gruposempar)): # Separando que nao Tem Par.
     if (Check_Grupos == False) and (Check_Isolados == False):
         Grupos_Isolados.append(gruposempar[g0])
 
-for e0 in range(len(GrupoInicial)): ### Separando os Grupos
+for e0 in range(len(GrupoInicial)):  # Separando os Grupos
     modificador = GrupoInicial[e0]
     for e1 in range(len(modificador)):
         modificador_1 = modificador[e1]
         Check_Grupos = False
-        for e2 in range (len(gruposempar)):
+        for e2 in range(len(gruposempar)):
             if modificador_1 == gruposempar[e2]:
                 Check_Grupos = True
         for e3 in range(len(Total)):
@@ -198,6 +228,9 @@ for e4 in range(len(Grupos_Isolados)):
         Total.append(Grupos_Isolados[e4])
 
 ################################################ - SAIDA COM SIMPLIFICAÇÃO - ###########################################
+
+if len(Total) == 0:
+    check5 = False
 if check5 == True:
     print('Saida Simplificada: ', end='')
     for k5 in range(len(entradas)):
@@ -207,7 +240,7 @@ if check5 == True:
             print(',' + entradas[k5] + '} = ', end='')
         else:
             print(',' + entradas[k5], end='')
-    for k6 in range (len(Total)):
+    for k6 in range(len(Total)):
         modificador = Total[k6]
         for k7 in range(len(entradas)):
             if modificador[k7] == 0:
